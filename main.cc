78af53a0e8de7551172bae38ede7da4b8d26f691
devinrankin <devinrankin@proton.me>
2025-08-09 19:49:42 -0400

    Updated main to include graphics implementation. ROMs that I've tested and work: IBM logo display, corax89 test ROM, Tetris, and Pong.

--- main.cc
+++ main.cc
@@ -1,14 +1,33 @@
 #include <memory>
 #include "chip8.hh"
+#include "graphics.hh"
+#include <SDL3/SDL.h>
+
+// Converting our Chip-8 display data to something readable by SDL
+void c8buff_to_rgba(const uint8_t* c8buff, uint32_t* out_buff, int width, int height) {
+    for(int i = 0; i < width * height; i++) {
+	out_buff[i] = c8buff[i] ? 0xFFFFFFFF : 0x000000FF;
+    }
+}
+
 
 int main(int argc, char** argv) {
     Chip8 chip8;
+    Graphics gfx;
+
+    uint32_t sdl_display[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
     const char* filename = argv[1];
     chip8.load_rom(filename);
+    
+    int pitch = sizeof(uint32_t) * DISPLAY_WIDTH;
 
     bool running = true;
     while(running) {
+	gfx.handle_input(chip8.keypad, running);
 	chip8.emulate_cycle();
+
+	c8buff_to_rgba(chip8.display, sdl_display, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	gfx.render(sdl_display, pitch);
     }
 }
